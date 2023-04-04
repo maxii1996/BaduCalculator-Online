d29cccde9adc575a4981523413d1383a2d2e3253
Maxi Martínez <maxinm@live.com>
2023-04-03 23:45:08 -0300

    test

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ document.getElementById('reset-quick-text-2-btn').addEventListener('click', rese
 document.getElementById('reset-quick-text-3-btn').addEventListener('click', resetQuickText3);
 
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}
+
+
+
 let productoId = 0;
 
 function agregarProducto() {
